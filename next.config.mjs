c312e14a45c9d6c1ce2ae606fda14bc302520ffa
SamGu-NRX <sgu07966@gmail.com>
2024-09-21 15:35:07 -0500

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,51 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import withSvgr from '@svgr/webpack';
+
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+
+  webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgo: true, // Disable SVGO optimization if you encounter issues
+            },
+          },
+        ],
+      });
+      return config;
+  },
+};
 
 export default nextConfig;
+
+// webpack(config) {
+//   config.module.rules.push({
+//     test: /\.svg$/,
+//     use: [
+//       {
+//         loader: '@svgr/webpack',
+//         options: {
+//           svgo: true, // Disable SVGO optimization if you encounter issues
+//         },
+//       },
+//     ],
+//   });
+//   return config;
+// },
+
